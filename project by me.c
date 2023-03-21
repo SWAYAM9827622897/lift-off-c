6c54e626578a0d0138e7ebee8f63914029240d4d
SWAYAM9827622897 <127418221+SWAYAM9827622897@users.noreply.github.com>
2023-03-22 01:40:07 +0530

    Add files via upload

---
+++ project by me.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+	printf("my name is swayam swarup panda.currently i am pursuing my bachelor of technology in veer surendra sai university of technology in the department of electronics & tele communication");
+	return 0;
+
+}
